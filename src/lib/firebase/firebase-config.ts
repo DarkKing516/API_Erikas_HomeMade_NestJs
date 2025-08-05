bad65dfd7e1ae731969897b4b75d3119545c9971
Jhon Contreras <jhomai7020@gmail.com>
2025-08-05 13:43:22 -0500

    Feat(FirebaseConfig): - se agrega bucket para manejo de imagenes

--- src/lib/firebase/firebase-config.ts
+++ src/lib/firebase/firebase-config.ts
@@ -1,3 +1,4 @@
+/*
 // lib/firebase-config.ts
 import * as admin from 'firebase-admin';
 
@@ -8,3 +9,17 @@ admin.initializeApp({
 const firestore = admin.firestore();
 
 export { firestore };
+*/
+
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
+
+const serviceAccount = require('../../../erika-s-homemade-firebase-adminsdk.json') as ServiceAccount;
+
+admin.initializeApp({
+    credential    : admin.credential.cert(serviceAccount),
+    storageBucket : 'gs://erika-s-homemade.appspot.com',
+});
+
+export const firestore = admin.firestore();
+export const bucket    = admin.storage().bucket();
